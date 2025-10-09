27f7d47d6d586a6b8341943f9d94e1c2ea9a473e
Hema Puchakayala <puchakayalahema@gmail.com>
2025-10-08 19:46:31 -0500

    DriftingUserSampler
    Updated Drifting User Sampler

--- src/main/main.py
+++ src/main/main.py
@@ -1,3 +1,4 @@
+#%%
 # -*- coding: utf-8 -*-
 """
 Author: Hema Chandra Puchakayala
@@ -5,22 +6,31 @@ Date: 2025-09-24
 Version: 1.0
 """
 
-# Commented out IPython magic to ensure Python compatibility.
-# %pip install ipynb
-
-!pip install --upgrade --no-cache-dir recsim
-
-from google.colab import drive
-drive.mount('/content/drive')
 
 import os
-os.chdir("/content/drive/MyDrive/Colab Notebooks")
+import sys
+# project_root = "/Users/hema/Desktop/GWU/Aug_2025/Capstone/fall-2025-group12"
+os.chdir('/../..')
+# sys.path.insert(0, '/Users/hema/Desktop/GWU/Aug_2025/Capstone/fall-2025-group12')
+from src.component.environment import * 
 
-import ipynb.fs.full.Environment as env
-
-sampler = env.DriftingDocumentSampler(env.categories, alpha=0.05, a=2, b=5 )
+print('Generating Documents')
+print('-'*50)
+sampler = DriftingDocumentSampler(categories, alpha=0.05, a=2, b=5 )
 docs = sampler.sample_documents(100)
 
 for d in docs:
   print(d.create_observation())
 
+print('='*50)
+
+print('Generating Users')
+print('-'*50)
+sampler = DriftingUserSampler(categories, alpha=0.05, a=2, b=5 )
+users = sampler.sample_users(100)
+
+for u in users:
+  print(u.create_observation())
+
+print('='*50)
+# %%
